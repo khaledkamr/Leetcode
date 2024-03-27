385896ab5d2065c025b378ef413e31d8a7538545
khaledkamr <200013171@must.edu.eg>
2024-03-27 17:57:54 +0200

    With C++

---
+++ Subarray Product Less Than K.cpp
@@ -0,0 +1,24 @@
+class Solution 
+{
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) 
+    {
+        int n = nums.size();
+        int count = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            int pro = 1;
+            for(int j = i; j < n; j++)
+            {
+                pro *= nums[j];
+                if(pro >= k)
+                {
+                    break;
+                }
+                count++;
+            }
+        }
+        return count;
+    }
+};
