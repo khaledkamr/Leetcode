239105937409aeb81277e6dee959e47ccbc8ee3e
khaledkamr <200013171@must.edu.eg>
2024-04-05 11:22:34 +0200

    With C++

---
+++ Make The String Great.cpp
@@ -0,0 +1,21 @@
+class Solution 
+{
+public:
+    string makeGood(string s) 
+    {
+        string res;
+
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(res != "" && abs(s[i] - res[res.size() - 1]) == 32)
+            {
+                res.pop_back();
+            }
+            else
+            {
+                res.push_back(s[i]);
+            }
+        }
+        return res;
+    }
+};
