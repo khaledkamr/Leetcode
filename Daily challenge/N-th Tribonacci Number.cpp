3e981faed4ddeda281c2270c773361ad57c4610c
khaledkamr <200013171@must.edu.eg>
2024-04-24 08:41:46 +0200

    Easy C++

---
+++ Daily challenge/N-th Tribonacci Number.cpp
@@ -0,0 +1,19 @@
+class Solution 
+{
+public:
+    int tribonacci(int n) 
+    {
+        if(n == 0) return 0;
+        else if(n == 1 || n == 2) return 1;
+
+        int t0 = 0, t1 = 1, t2 = 1;
+        while(n-- > 2)
+        {
+            int temp = t2;
+            t2 = t0 + t1+ t2;
+            t0 = t1;
+            t1 = temp;
+        }
+        return t2;
+    }
+};
