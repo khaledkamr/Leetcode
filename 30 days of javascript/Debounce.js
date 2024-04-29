40b242f432c5d54c2ae32636184a40ea1774d0c8
khaledkamr <200013171@must.edu.eg>
2024-04-29 11:04:08 +0300

    Medium

---
+++ 30 days of javascript/Debounce.js
@@ -0,0 +1,9 @@
+var debounce = function(fn, t = 1000) 
+{
+    let timer;
+    return function(...args) 
+    {
+        clearTimeout(timer);
+        timer = setTimeout(() => fn(...args), t);
+    }
+};
