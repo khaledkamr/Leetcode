d1030c5141c1323ce7f1195bba98f7bee0c7c357
khaledkamr <200013171@must.edu.eg>
2024-05-04 09:21:38 +0300

    Easy

---
+++ 30 days of javascript/Array Wrapper.js
@@ -0,0 +1,15 @@
+
+var ArrayWrapper = function(nums) 
+{
+    this.array = nums;    
+};
+
+ArrayWrapper.prototype.valueOf = function() 
+{
+    return this.array.reduce((sum, num) => sum + num, 0);
+}
+
+ArrayWrapper.prototype.toString = function() 
+{
+    return JSON.stringify(this.array);    
+}
