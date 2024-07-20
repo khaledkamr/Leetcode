7375cab797220193a4b258fec0f0922c6d9879d7
khaledkamr <200013171@must.edu.eg>
2024-07-20 15:23:26 +0300

    Medium Python3

---
+++ Daily challenge/1605. Find Valid Matrix Given Row and Column Sums.py
@@ -0,0 +1,20 @@
+class Solution:
+    def restoreMatrix(self, rowSum: list[int], colSum: list[int]) -> list[list[int]]:
+        numRows = len(rowSum)
+        numCols = len(colSum)
+        result = [[0] * numCols for _ in range(numRows)]
+
+        i, j = 0, 0
+
+        while i < numRows and j < numCols:
+            val = min(rowSum[i], colSum[j])
+            result[i][j] = val
+            rowSum[i] -= val
+            colSum[j] -= val
+
+            if rowSum[i] == 0:
+                i += 1
+            if colSum[j] == 0:
+                j += 1
+
+        return result
