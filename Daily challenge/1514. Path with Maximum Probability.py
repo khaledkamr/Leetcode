ba67946e241e4a7eb179f9eaf2b87bd366e18687
khaledkamr <200013171@must.edu.eg>
2024-08-31 13:54:26 +0300

    Medium Python3

--- Daily challenge/1514. Path with Maximum Probability.py
+++ Daily challenge/1514. Path with Maximum Probability.py
@@ -1,15 +1,8 @@
 class Solution:
-    def maxProbability(
-        self,
-        n: int,
-        edges: List[List[int]],
-        succProb: List[float],
-        start: int,
-        end: int,
-    ) -> float:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
         dist = [0] * n
         dist[start] = 1
-
+        
         for _ in range(n - 1):
             updated = False
             for i, (u, v) in enumerate(edges):
@@ -19,8 +12,7 @@ class Solution:
                 if dist[v] * succProb[i] > dist[u]:
                     dist[u] = dist[v] * succProb[i]
                     updated = True
-                    
             if not updated:
                 break
-
+        
         return dist[end]
