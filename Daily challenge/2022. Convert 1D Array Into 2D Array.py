1fbf944254876bd6761640abeb323de1c9754eb7
khaledkamr <200013171@must.edu.eg>
2024-09-01 12:03:24 +0300

    Easy Python3

---
+++ Daily challenge/2022. Convert 1D Array Into 2D Array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def construct2DArray(self, original: list[int], m: int, n: int) -> list[list[int]]:
+        if len(original) != m * n:
+            return []
+        res = []
+
+        for r in range(m):
+            start = r * n
+            end = start + n
+            res.append(original[start:end])
+
+        return res
