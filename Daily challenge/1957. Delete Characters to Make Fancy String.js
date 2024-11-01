67d286edf9604d53981f4e9a70011db8d8189d9b
khaledkamr <200013171@must.edu.eg>
2024-11-01 05:12:40 +0200

    Easy Javascript

---
+++ Daily challenge/1957. Delete Characters to Make Fancy String.js
@@ -0,0 +1,20 @@
+var makeFancyString = function (s) {
+  let result = "";
+  let count = 1;
+
+  result += s[0];
+
+  for (let i = 1; i < s.length; i++) {
+    if (s[i] === s[i - 1]) {
+      count++;
+    } else {
+      count = 1;
+    }
+
+    if (count < 3) {
+      result += s[i];
+    }
+  }
+
+  return result;
+};
