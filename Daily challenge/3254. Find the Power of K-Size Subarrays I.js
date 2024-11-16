16a36dfa0aac004206475476eb5349070f6e2efa
khaledkamr <200013171@must.edu.eg>
2024-11-16 06:37:35 +0200

    Medium Javascript

---
+++ Daily challenge/3254. Find the Power of K-Size Subarrays I.js
@@ -0,0 +1,20 @@
+var resultsArray = function (nums, k) {
+  const result = [];
+  function isConsecutiveAndSorted(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] !== arr[i - 1] + 1) {
+        return false;
+      }
+    }
+    return true;
+  }
+  for (let i = 0; i <= nums.length - k; i++) {
+    const subarray = nums.slice(i, i + k);
+    if (isConsecutiveAndSorted(subarray)) {
+      result.push(Math.max(...subarray));
+    } else {
+      result.push(-1);
+    }
+  }
+  return result;
+};
