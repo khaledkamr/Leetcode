5ce93ae9714bf672265efeb212537d79ac5d3826
Khaled Kamr <200013171@must.edu.eg>
2024-12-03 06:07:52 +0200

    Medium Javascript

---
+++ Daily challenge/2109. Adding Spaces to a String.js
@@ -0,0 +1,16 @@
+var addSpaces = function (s, spaces) {
+  let temp = "",
+    left = 0,
+    right = 0,
+    count = 0;
+  while (right < s.length) {
+    if (right === spaces[count]) {
+      temp += s.slice(left, right) + " ";
+      left = right;
+      count++;
+    }
+    right++;
+  }
+  temp += s.slice(left, right);
+  return temp;
+};
